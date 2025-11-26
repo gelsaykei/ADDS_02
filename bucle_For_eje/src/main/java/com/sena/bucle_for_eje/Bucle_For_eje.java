0db02a0b35de9ebdf3edd14d073c37c376be5aea
gelsaykei <gelsaykei@gmail.com>
2025-11-25 19:02:41 -0500

    bucle for

---
+++ bucle_For_eje/src/main/java/com/sena/bucle_for_eje/Bucle_For_eje.java
@@ -0,0 +1,25 @@
+package com.sena.bucle_for_eje;
+import java.util.Scanner;
+public class Bucle_For_eje {
+    static Scanner sc=new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        /*
+        int per, eda,ContM=0;
+        
+        System.out.print("Ingresa la cantidad de personas: ");
+        per = Integer.parseInt(sc.nextLine());
+        
+        for (int i = 1; i <= per; i++) {
+            System.out.println("Ingresa la edad de la persona n° "+i);
+            eda = Integer.parseInt(sc.nextLine());
+            if(eda >= 18){
+                ContM++;
+            }
+            System.out.println("");
+        }
+        System.out.println("Los mayores de edad son "+ContM);
+        */
+        
+    }
+}
